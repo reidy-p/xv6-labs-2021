13e706624ec322a32f269c0b1b636e16df160e95
reidy-p <paul-reidy@outlook.com>
2023-01-01 19:24:07 +0000

    Lab 3 WIP

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -11,10 +11,10 @@ uint64
 sys_exit(void)
 {
   int n;
-  if(argint(0, &n) < 0)
+  if (argint(0, &n) < 0)
     return -1;
   exit(n);
-  return 0;  // not reached
+  return 0; // not reached
 }
 
 uint64
@@ -33,7 +33,7 @@ uint64
 sys_wait(void)
 {
   uint64 p;
-  if(argaddr(0, &p) < 0)
+  if (argaddr(0, &p) < 0)
     return -1;
   return wait(p);
 }
@@ -44,11 +44,11 @@ sys_sbrk(void)
   int addr;
   int n;
 
-  if(argint(0, &n) < 0)
+  if (argint(0, &n) < 0)
     return -1;
-  
+
   addr = myproc()->sz;
-  if(growproc(n) < 0)
+  if (growproc(n) < 0)
     return -1;
   return addr;
 }
@@ -59,13 +59,14 @@ sys_sleep(void)
   int n;
   uint ticks0;
 
-
-  if(argint(0, &n) < 0)
+  if (argint(0, &n) < 0)
     return -1;
   acquire(&tickslock);
   ticks0 = ticks;
-  while(ticks - ticks0 < n){
-    if(myproc()->killed){
+  while (ticks - ticks0 < n)
+  {
+    if (myproc()->killed)
+    {
       release(&tickslock);
       return -1;
     }
@@ -75,12 +76,38 @@ sys_sleep(void)
   return 0;
 }
 
-
 #ifdef LAB_PGTBL
-int
-sys_pgaccess(void)
+int sys_pgaccess(void)
 {
-  // lab pgtbl: your code here.
+  struct proc *p = myproc();
+  uint64 va;
+  int n;
+  int bitmask = 0;
+  uint64 addr;
+
+  if (argaddr(0, &va) < 0)
+    return -1;
+
+  if (argint(1, &n) < 0)
+    return -1;
+
+  if (argaddr(2, &addr) < 0)
+    return -1;
+
+  for (int i = 0; i < n; i++)
+  {
+    pte_t *pte;
+    pte = walk(p->pagetable, va, 0);
+    if (*pte & PTE_A)
+    {
+      bitmask |= (1 << i);
+      // Reset the PTE_A
+      *pte &= ~(PTE_A);
+    }
+    va += PGSIZE;
+  }
+
+  copyout(p->pagetable, addr, (char *)&bitmask, sizeof(bitmask));
   return 0;
 }
 #endif
@@ -90,7 +117,7 @@ sys_kill(void)
 {
   int pid;
 
-  if(argint(0, &pid) < 0)
+  if (argint(0, &pid) < 0)
     return -1;
   return kill(pid);
 }
